6cc7a1af13a7df948d5dde37f53c82049fb26596
Paul <paul.huynh3@gmail.com>
2023-09-18 15:47:44 -0400

    Adding models

---
+++ Actor Isolation/Model/Student.swift
@@ -0,0 +1,12 @@
+//
+//  Student.swift
+//  Actor Isolation
+//
+//  Created by Paul Huynh on 2023-09-18.
+//
+
+import Foundation
+
+struct Student: Sendable {
+    let title: String
+}
---
+++ Actor Isolation/Model/Teacher.swift
@@ -0,0 +1,13 @@
+//
+//  Teacher.swift
+//  Actor Isolation
+//
+//  Created by Paul Huynh on 2023-09-18.
+//
+import Foundation
+
+actor Teacher {
+    let id = UUID()
+    var students: [Student] = []
+}
+
