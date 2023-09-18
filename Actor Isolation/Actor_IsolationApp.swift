4db78deff892d4c4a2f9d2e2932a05aa599908bb
Paul <paul.huynh3@gmail.com>
2023-09-18 15:15:52 -0400

    Initial Commit

---
+++ Actor Isolation/Actor_IsolationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Actor_IsolationApp.swift
+//  Actor Isolation
+//
+//  Created by Paul Huynh on 2023-09-18.
+//
+
+import SwiftUI
+
+@main
+struct Actor_IsolationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
